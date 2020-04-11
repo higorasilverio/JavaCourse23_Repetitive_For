68e36f2543b90ad46b34d7d94f083683feaa12e2
higorasilverio <higorasilverio@outlook.com>
2020-04-11 16:28:49 -0300

    Calculating the dividers - Final

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("*** Calculating the dividers ***");
+		System.out.println();
+		
+		System.out.print("Enter the number to calculate the dividers: ");
+		int number = scanner.nextInt();
+		for (int i = 1; i <= number; i++) {
+			if(number != 0 && number % i == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		scanner.close();
+		
+	}
+
+}
